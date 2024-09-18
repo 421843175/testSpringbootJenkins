6c680631e42a5bfab054d5ee7b6dda4c5b7c61a1
421843175 <421843175@qq.com>
2024-09-18 20:45:02 +0800

    four

--- src/main/java/com/jupiter/controller/IndexController.java
+++ src/main/java/com/jupiter/controller/IndexController.java
@@ -10,6 +10,6 @@ public class IndexController {
 
     @GetMapping("/")
     public String toIndex(){
-        return "hello hi hi hi hinew hello";
+        return "the hehehehehehehe hiiiiiii";
     }
 }
